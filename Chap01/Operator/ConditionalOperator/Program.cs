ff1104895e517e5a2139e3d51425d71b345bedcf
goodluck3586 <goodluck3586@gsm.hs.kr>
2019-08-13 20:57:20 +0900

    연산자

---
+++ Chap01/Operator/ConditionalOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("정수를 입력하세요 : ");
+            int a = int.Parse(Console.ReadLine());
+            string result = a % 2 == 0 ? "짝수" : "홀수";
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Chap01/Operator/NullConditionalOperator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullConditionalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Null 병합 연산자 : 변수/객체의 간결한 null 검사
+            int? a = null;
+            Console.WriteLine($"{a ?? 0}");
+
+            a = 99;
+            Console.WriteLine($"{a ?? 0}");
+            Console.WriteLine();
+
+            // Null 조건부 연산자
+            ArrayList arr = null;
+            arr?.Add("야구");  // a가 null을 반환하므로 Add() 메소드는 호출되지 않는다.
+            Console.WriteLine($"Count : {arr?.Count}");
+            Console.WriteLine($"arr[0] : {arr?[0]}");
+            Console.WriteLine();
+
+            arr = new ArrayList();
+            arr?.Add("야구");  
+            Console.WriteLine($"Count : {arr?.Count}");
+            Console.WriteLine($"arr[0] : {arr?[0]}");
+            Console.WriteLine();
+        }
+    }
+}
