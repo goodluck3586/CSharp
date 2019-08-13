26508e047a70db1a4f5a9cf1402ee7d5a24ad3e7
goodluck3586 <goodluck3586@gsm.hs.kr>
2019-08-13 10:48:08 +0900

    add hello world

---
+++ Chap01/HelloWorld/CSharpBasic/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasic
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
