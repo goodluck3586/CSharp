177ad6cd77897021f269f6cb2f5d0d071a5320ec
goodluck3586 <goodluck3586@gsm.hs.kr>
2020-06-12 08:43:45 +0900

    gg

--- AfterSchoolClass2020/AfterSchoolClass2020/Program.cs
+++ AfterSchoolClass2020/AfterSchoolClass2020/Program.cs
@@ -10,6 +10,24 @@ namespace AfterSchoolClass2020
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Short Cut Keys");
+            #region Short Cut Keys
+            // Shift : Delete : 한 줄 지우기 
+
+            // [F9] : BreakPoint 만들기/지우기
+            // [F10] : Step Over, [F11] : Step Into (the next method)
+
+            // [F12] : 해당 코드 정의 보여주기
+
+            // *** 주석 ***
+            // Ctrl + KC : 주석 달기
+            // Ctrl + KU : 주석 없애기
+
+            // Ctrl + MO : 모든 코드 접기
+            // Ctrl + MP : 모든 코드 펴기
+
+
+            #endregion
         }
     }
 }
